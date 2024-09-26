3a601c491362eec0f454acd6afb2696712a3bc81
JuanCarlosIsordiaB <juancarlosisordiab@gmail.com>
2024-09-25 21:32:36 -0600

    crud cars

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,10 +1,39 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
+import { CarsService } from './cars.service';
 
 @Controller('cars')
 export class CarsController {
-    
+
+
+    constructor(
+        private readonly carsService: CarsService
+    ){}
+    //Read
     @Get()
     getAllCars(){
-        return 'All cars';
+        return this.carsService.findAll();
     }
+
+    
+    @Get(':id')
+    getCarsById( @Param('id', ParseIntPipe) id: number ){    
+        return this.carsService.findOne(id); ;
+    }
+
+
+    @Post() //Crea
+    createCar( @Body() body: any ){
+        
+    }
+    @Patch(':id') // Actualiza
+    updateCar( @Param('id', ParseIntPipe) id:number,  @Body() body: any ){
+        return `Car with id ${body.id} updated`;
+    }
+
+    @Delete(':id') // Elimina
+    deleteCar( @Param('id', ParseIntPipe) id: number ){
+        return `Car with id ${id} deleted`;
+    }
+    
+    
 }
